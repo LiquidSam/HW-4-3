6875f319111d8a8f40c7aaa75bdea62fa6564f91
liquidsamlol <liquidsamlol@gmail.com>
2018-10-05 01:18:53 +0300

    done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alpha = "abcdefghijklmnopqrstuvwxyz";
+
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                Console.Write(alpha[alpha.Length - i - 1]);
+            }
+        }
+    }
+}
